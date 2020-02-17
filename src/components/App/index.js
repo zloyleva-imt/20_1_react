ef1347a959adec16975be25ad9fd2348760cad21
zloyleva <zloyleva@gmail.com>
2020-02-17 19:03:05 +0200

    create maket

---
+++ src/components/App/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const App = () => (
+    <div className="App">
+        <h1>Tasks List App</h1>
+        <form action="">
+            <input type="text"/>
+            <button>Add task</button>
+        </form>
+        <div>
+            <ul>
+                <li>Task #1</li>
+                <li>Task #2</li>
+                <li>Task #3</li>
+                <li>Task #4</li>
+                <li>Task #5</li>
+            </ul>
+        </div>
+    </div>
+);
+
+export default App;
